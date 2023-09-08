dbacd96fe92fc652a2c2a0d5fac40440b2b045a4
ADMIN <maks.volontirov@gmail.com>
2023-09-08 19:41:20 +0300

    fix 4

--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -1,5 +1,6 @@
 package com.es.core.model.phone;
 
+import com.es.core.model.rowmapper.PhoneRowMapper;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -10,14 +11,15 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
 public class JdbcPhoneDao implements PhoneDao {
     private final static String SELECT_PHONE_QUERY = "select * from phones where id = ?";
-    private final static String SELECT_COLOR_JOIN_QUERY = "select colors.id, colors.code from colors join phone2color" +
-            " on phone2color.colorId = colors.id where phone2color.phoneId = ?";
     private final static String DELETE_PHONE2COLOR_QUERY = "delete from phone2color where phoneId = ?";
     private final static String INSERT_PHONE2COLOR_QUERY = "insert into phone2color (phoneId, colorId) values(:phoneId, :colorId)";
     private final static String SELECT_PHONE_OFFSET_QUERY = "select * from phones offset ? limit ?";
@@ -38,26 +40,19 @@ public class JdbcPhoneDao implements PhoneDao {
     private JdbcTemplate jdbcTemplate;
 
     public Optional<Phone> get(final Long key) {
+        Object[] keyArg = new Object[]{key};
         Optional<Phone> phone = jdbcTemplate.query(SELECT_PHONE_QUERY,
-                        new Object[]{key}, new BeanPropertyRowMapper(Phone.class))
+                        keyArg, new PhoneRowMapper(jdbcTemplate))
                 .stream()
                 .findFirst();
-        if (phone.isPresent()) {
-            Set<Color> colors = getColorSet(phone.get());
-            phone.get().setColors(colors);
-        }
         return phone;
     }
 
-    private Set<Color> getColorSet(final Phone phone) {
-        return jdbcTemplate.query(SELECT_COLOR_JOIN_QUERY,
-                        new Object[]{phone.getId()}, new BeanPropertyRowMapper<>(Color.class))
-                .stream()
-                .collect(Collectors.toSet());
-    }
+
 
     public void save(final Phone phone) {
-        if (jdbcTemplate.query(SELECT_PHONE_QUERY, new Object[]{phone.getId()},
+        Object[] phoneIdArg = new Object[]{phone.getId()};
+        if (jdbcTemplate.query(SELECT_PHONE_QUERY, phoneIdArg,
                 new BeanPropertyRowMapper<>(Phone.class)).isEmpty()) {
             SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
             Number number = insert.withTableName(PHONES_TABLE).usingGeneratedKeyColumns(ID_COLUMN)
@@ -73,22 +68,18 @@ public class JdbcPhoneDao implements PhoneDao {
     private void saveColors(final Phone phone) {
         jdbcTemplate.update(DELETE_PHONE2COLOR_QUERY, phone.getId());
         List<SqlParameterSource> parameterSources = phone.getColors().stream()
-                .map(color -> {
-                    return new MapSqlParameterSource(Map.of(PHONE_ID_COLUMN, phone.getId(),
-                            COLOR_ID_COLUMN, color.getId()));
-                }).collect(Collectors.toList());
+                .map(color -> new MapSqlParameterSource(Map.of(PHONE_ID_COLUMN, phone.getId(),
+                            COLOR_ID_COLUMN, color.getId()))
+                ).collect(Collectors.toList());
         NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(jdbcTemplate.getDataSource());
         template.batchUpdate(INSERT_PHONE2COLOR_QUERY,
                 parameterSources.toArray(new SqlParameterSource[0]));
     }
 
     public List<Phone> findAll(int offset, int limit) {
-        List<Phone> phones = jdbcTemplate.query(SELECT_PHONE_OFFSET_QUERY, new Object[]{offset, limit},
-                new BeanPropertyRowMapper(Phone.class));
-        for (Phone phone : phones) {
-            Set<Color> colors = getColorSet(phone);
-            phone.setColors(colors);
-        }
+        Object[] offsetLimitArg = new Object[]{offset, limit};
+        List<Phone> phones = jdbcTemplate.query(SELECT_PHONE_OFFSET_QUERY, offsetLimitArg,
+                new PhoneRowMapper(jdbcTemplate));
         return phones;
     }
 }
