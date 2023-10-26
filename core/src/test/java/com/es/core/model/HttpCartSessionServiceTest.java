b9dc96cb70d251c5a8052564d0b0ecaf65a9b099
ADMIN <maks.volontirov@gmail.com>
2023-10-26 18:20:56 +0300

    task 7 cartService test fix1

--- core/src/test/java/com/es/core/model/HttpCartSessionServiceTest.java
+++ core/src/test/java/com/es/core/model/HttpCartSessionServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.ObjectFactory;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -23,8 +24,6 @@ import static org.mockito.Mockito.when;
 
 public class HttpCartSessionServiceTest {
     private Cart cart;
-    @Mock
-    private Map<Phone, Long> phones;
     @Mock
     private ObjectFactory<Cart> cartObjectFactory;
     @Mock
@@ -38,28 +37,26 @@ public class HttpCartSessionServiceTest {
         cart = new Cart();
         cart.setPhones(new HashMap<>());
         MockitoAnnotations.openMocks(this);
+        when(cartService.getCart()).thenReturn(cart);
+        Phone phone = new Phone();
+        phone.setPrice(BigDecimal.valueOf(100L));
+        when(phoneDao.get(any())).thenReturn(Optional.of(phone));
+        Stock stock = new Stock();
+        stock.setReserved(1);
+        stock.setStock(10);
+        when(stockDao.getAvailableStock(any())).thenReturn(stock);
     }
     @Test
     public void cartServiceAddPhoneTest() {
-        when(cartService.getCart()).thenReturn(cart);
-        when(phoneDao.get(any())).thenReturn(Optional.of(new Phone()));
-        Stock stock = new Stock();
-        stock.setReserved(1);
-        stock.setStock(10);
-        when(stockDao.getAvailableStock(any())).thenReturn(stock);
         cartService.addPhone(1001L, 2L);
         Assert.assertEquals(2L, cartService.getCart().getTotalQuantity().longValue());
+        Assert.assertEquals(200L, cart.getTotalPrice().longValue());
     }
     @Test
     public void cartServiceClear() {
-        when(cartService.getCart()).thenReturn(cart);
-        when(phoneDao.get(any())).thenReturn(Optional.of(new Phone()));
-        Stock stock = new Stock();
-        stock.setReserved(1);
-        stock.setStock(10);
-        when(stockDao.getAvailableStock(any())).thenReturn(stock);
         cartService.addPhone(1001L, 2L);
         cartService.clear();
         Assert.assertEquals(0L, cartService.getCart().getTotalQuantity().longValue());
+        Assert.assertEquals(0L, cartService.getCart().getTotalPrice().longValue());
     }
 }
